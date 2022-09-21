6517d6832aa99ac5914ba54fd5d0b0bae088fb0b
FlorianZasada <florian.zasada@gmail.com>
2022-09-21 09:33:07 +0200

    added vor td

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ function renderBots(doc) {
     let tr = document.createElement('tr')
     let name = document.createElement('td')
     let status = document.createElement('td')
+    let vor = document.createElement('td')
     let last_status = document.createElement('td')
 
     tr.setAttribute('data-id', doc.id)
@@ -31,27 +32,50 @@ function renderBots(doc) {
 
     name.textContent = doc.data().id.toUpperCase()+" / "+doc.data().name.toUpperCase()
     status.textContent = doc.data().status.toUpperCase()
+    vor.textContent = "vor " +getVorTime(last_status) + "sek".toUpperCase()
     last_status.textContent = doc.data().last_state
 
     if(is_inactive_activeness(doc.data().last_state)) {
         name.style.color = "#ff0d00"
         status.style.color = "#ff0d00"
         last_status.style.color = "#ff0d00"
+        vor.style.color = "#ff0d00"
     } else {
         name.style.color = "#0afa2e"
         status.style.color = "#0afa2e"
         last_status.style.color = "#0afa2e"
+        vor.style.color = "#0afa2e"
     }
-
-    console.log(name, status, last_status)
 
     tr.appendChild(name)
     tr.appendChild(status)
+    tr.appendChild(vor)
     tr.appendChild(last_status)
 
     botlist.append(tr)
 
 };
+
+function getVorTime(time) {
+    var today = new Date();
+    const dt_sections= time.split(',');
+    const date_sections = dt_sections[0].split('.')
+    day = date_sections[0].trim()
+    month = date_sections[1].trim()
+    year = date_sections[2].trim()
+    const time_sections = dt_sections[1].split(':');
+    hour = time_sections[0].trim()
+    minute = time_sections[1].trim()
+    second = time_sections[2].trim()
+
+    // 01.01.2022, 00:00:00
+    ls_dt = new Date(year, parseInt(month)-1, day, hour, minute, second, 0);
+
+    var dif = ls_dt.getTime() - today.getTime();
+    var seconds_dif = dif / 1000;
+    var seconds = Math.abs(seconds_dif);
+    return seconds
+}
 
 function is_inactive_activeness(time) {
     var today = new Date();
